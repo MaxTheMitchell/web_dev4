bff4f623f9e4907c1ac4736a46243de9e6a189b7
Max Mitchell <rat2043@gmail.com>
2020-04-26 15:54:52 -0700

    init commit

---
+++ manufacturers/index.php
@@ -0,0 +1,23 @@
+<?php
+require '../config/db.php';
+$db = new mysqli(
+  $database_hostname,
+  $database_username,
+  $database_password,
+  $database_db_name,
+  $database_port
+);
+if ($db->connect_error) {
+  die("Error: Could not connect to database. " . $db->connect_error);
+}
+$sql = "SELECT * FROM manufacturers ORDER BY name;";
+$manufacturers = $db->query($sql);
+?>
+<?php include '../header.php'; ?>
+    <h2>Manufacturers</h2>
+    <ul id="categories">
+    <?php while($manufacture = $manufacturers->fetch_assoc()) { ?>
+      <li><h3><a href="/manufacturers/show.php?id=<?= $manufacture["id"] ?>"><?= $manufacture['name'] ?></a></h3></li>
+    <?php } ?>
+    </ul>
+<?php include '../footer.php'; ?>
